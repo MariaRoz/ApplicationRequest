c9d39b8b0ec8f9e5357bd2f047470f393f01454f
MariaRoz <Rozmasia@mail.ru>
2017-09-19 19:01:46 +0300

    newcommit

--- src/main/java/com/example/demo/ApplicationRequestApplication.java
+++ src/main/java/com/example/demo/ApplicationRequestApplication.java
@@ -7,29 +7,43 @@ import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @SpringBootApplication
 public class ApplicationRequestApplication {
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(ApplicationRequestApplication.class, args);
-		CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
-		try {
-			httpclient.start();
-			HttpGet request = new HttpGet("http://localhost:8080/Servlet");
-//			ExecutorService exec = Executors.newFixedThreadPool(4);
-			for (int i = 0; i <1000 ; i++) {
-						Future<HttpResponse> future = httpclient.execute(request, null);
-						HttpResponse response = future.get();
-						System.out.println("Response: " + response.getStatusLine());
-						System.out.println("Shutting down");
-					}
-
-		} finally {
-			httpclient.close();
-		}
-		System.out.println("Done");
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationRequestApplication.class, args);
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 4; ++i) {
+            execute.execute(new MyTask());
+        }
+        execute.shutdown();
+    }
 }
 
+class MyTask implements Runnable {
+    public void run() {
+        execHttpRequest();
+    }
 
+    private void execHttpRequest() {
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
+        try {
+            httpclient.start();
+            HttpGet request = new HttpGet("http://localhost:8080/Servlet");
+            for (int i = 0; i < 250; i++) {
+                Future<HttpResponse> future = httpclient.execute(request, null);
+                HttpResponse response = future.get();
+                System.out.println("Response: " + response.getStatusLine());
+                System.out.println("Shutting down");
+//				System.out.println(Thread.currentThread().getName());
+            }
+            httpclient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done");
+    }
+}
